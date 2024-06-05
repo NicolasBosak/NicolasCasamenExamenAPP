87f8e267f7fb86ecd940b6f5ab12a566a25cec49
Nicolas Casamen <sebastian.casamen@udla.edu.ec>
2024-06-05 12:01:03 -0500

    Se agrega la navegacion por paginas y una imagen en la pagina principal.

---
+++ NCAppNotas/Views/NCConteoCaracteres.xaml.cs
@@ -0,0 +1,9 @@
+namespace NCAppNotas.Views;
+
+public partial class NCConteoCaracteres : ContentPage
+{
+	public NCConteoCaracteres()
+	{
+		InitializeComponent();
+	}
+}
--- NCAppNotas/Views/NCPaginaPrincipal.xaml.cs
+++ NCAppNotas/Views/NCPaginaPrincipal.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+
 namespace NCAppNotas.Views;
 
 public partial class NCPaginaPrincipal : ContentPage
@@ -5,5 +8,21 @@ public partial class NCPaginaPrincipal : ContentPage
 	public NCPaginaPrincipal()
 	{
 		InitializeComponent();
-	}
+        Routing.RegisterRoute(nameof(NCAllNotesPage), typeof(NCAllNotesPage));
+        Routing.RegisterRoute(nameof(NCConteoCaracteres), typeof(NCConteoCaracteres));
+        Routing.RegisterRoute(nameof(NCAboutPage), typeof(NCAboutPage));
+    }
+   
+    async void OnButtonClicked(object sender, EventArgs args)
+    {
+        await Shell.Current.GoToAsync(nameof(NCAllNotesPage));
+    }
+    async void ContarClicked(object sender, EventArgs args)
+    {
+        await Shell.Current.GoToAsync(nameof(NCConteoCaracteres));
+    }
+    async void InformacionClicked(object sender, EventArgs args)
+    {
+        await Shell.Current.GoToAsync(nameof(NCAboutPage));
+    }
 }
